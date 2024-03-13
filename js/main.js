8b13494d09c1abce97f08ec4926ff1f07cde0d47
Kat-girl <podkaterina@gmail.com>
2024-03-13 18:15:59 +0300

    Подключен главный скрипт, добавлен модуль  для неактивного состояния страницы

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import {setInactivePageMode} from './setInactivePageMode.js';
+
+setInactivePageMode();
---
+++ js/setInactivePageMode.js
@@ -0,0 +1,25 @@
+const adForm = document.querySelector('.ad-form');
+const adFormHeader = document.querySelector('.ad-form-header');
+const adFormElements = document.querySelectorAll('.ad-form__element');
+const mapFilterForm = document.querySelector('.map__filters');
+const mapFilters = document.querySelectorAll('.map__filter');
+const mapFeatures = document.querySelector('.map__features');
+
+
+const setInactivePageMode = () => {
+  adForm.classList.add('ad-form--disabled');
+  adFormHeader.disabled = true;
+  for (const element of adFormElements) {
+    element.disabled = true;
+  }
+
+  mapFilterForm.classList.add('ad-form--disabled');
+  for (const filter of mapFilters) {
+    filter.disabled = true;
+  }
+  mapFeatures.disabled = true;
+};
+
+export {setInactivePageMode};
+
+
