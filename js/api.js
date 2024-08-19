5744bb6d91b9c8563e691499ef1ac304e3740f44
Kat-girl <podkaterina@gmail.com>
2024-08-19 16:02:54 +0300

    Добавлена фильтрация по количеству комнат, гостей

--- js/api.js
+++ js/api.js
@@ -11,6 +11,7 @@ const getData = (onSuccess, onFail) => {
     })
     .then((ads) => {
       onSuccess(ads);
+      console.log(ads);
     })
     .catch(() => onFail('Не удалось загрузить данные с сервера'));
 };
--- js/filters.js
+++ js/filters.js
@@ -1,22 +1,103 @@
 import {clearLayer} from './map.js';
 
+// Фильтрация по типу жилья
 const housingType = document.querySelector('#housing-type');
-console.log(housingType.value);
 
 const filterHousingType = (ads) => {
   if (housingType.value !== 'any') {
-    const filteredArr = ads.filter((ad) => ad.offer.type === housingType.value);
-    return filteredArr;
+    return ads.filter((ad) => ad.offer.type === housingType.value);
   } else {
     return ads;
   }
 };
 
-const onHousingTypeChange = (cb) => {
-  housingType.addEventListener('change', () => {
-    clearLayer();
-    cb();
-  });
+// Фильтрация по цене
+const housingPrice = document.querySelector('#housing-price');
+
+const filterHousingPrice = (ads) => {
+  if (housingPrice.value !== 'any') {
+    if (housingPrice.value === 'low') {
+      return ads.filter((ad) => ad.offer.price <= 10000);
+    } else if (housingPrice.value === 'middle') {
+      return ads.filter((ad) => ad.offer.price > 10000 && ad.offer.price < 50000);
+    } else {
+      return ads.filter((ad) => ad.offer.price >= 50000);
+    }
+  } else {
+    return ads;
+  }
 };
 
-export {filterHousingType, onHousingTypeChange};
+// Фильтрация по количеству комнат
+const housingRooms = document.querySelector('#housing-rooms');
+
+const filterHousingRooms = (ads) => {
+  if (housingRooms.value !== 'any') {
+    return ads.filter((ad) => ad.offer.rooms === +housingRooms.value);
+  } else {
+    return ads;
+  }
+};
+
+// Фильтрация по количеству гостей
+const housingGuests = document.querySelector('#housing-guests');
+
+const filterHousingGuests = (ads) => {
+  if (housingGuests.value !== 'any') {
+    return ads.filter((ad) => ad.offer.guests === +housingGuests.value);
+  } else {
+    return ads;
+  }
+};
+
+// Фильтрация по фичам
+const getFeatureRank = (ad) => {
+  const filterWiFi = document.querySelector('#filter-wifi');
+  const filterDishwasher = document.querySelector('#filter-dishwasher');
+  const filterParking = document.querySelector('#filter-parking');
+  const filterWasher = document.querySelector('#filter-washer');
+  const filterElevator = document.querySelector('#filter-elevator');
+  const filterConditioner = document.querySelector('#filter-conditioner');
+
+  let rank = 0;
+  if (ad.offer.features.includes(filterWiFi.value)) {
+    rank += 1;
+  }
+  if (ad.offer.features.includes(filterDishwasher.value)) {
+    rank += 1;
+  }
+  if (ad.offer.features.includes(filterParking.value)) {
+    rank += 1;
+  }
+  if (ad.offer.features.includes(filterWasher.value)) {
+    rank += 1;
+  }
+  if (ad.offer.features.includes(filterElevator.value)) {
+    rank += 1;
+  }
+  if (ad.offer.features.includes(filterConditioner.value)) {
+    rank += 1;
+  }
+  return rank;
+};
+
+const compareAds = (adA, adB) => {
+  const rankA = getFeatureRank(adA);
+  const rankB = getFeatureRank(adB);
+  return rankB - rankA;
+};
+
+const applyFilters = (ads) => filterHousingGuests(filterHousingRooms(filterHousingPrice(filterHousingType(ads))));
+
+const onFilterChange = (cb) => {
+  const mapFilters = document.querySelectorAll('.map__filter');
+  for (const filter of mapFilters) {
+    filter.addEventListener('change', () => {
+      clearLayer();
+      cb();
+    });
+  }
+};
+
+
+export {onFilterChange, applyFilters};
